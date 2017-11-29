b1e31705bbd0292e9874fe2fc4cf6ee5d4d7136e
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2017-11-29 08:01:58 +0200

    First Commit

---
+++ Student prerequisite course system/DataStructures/SmallContainers.cs
@@ -0,0 +1,18 @@
+﻿
+public struct Course
+{
+    public string Name;
+}
+
+public struct Student
+{
+    public string Name, Password;
+    public int ID;
+    public Course[] RegisteredCourses;
+    public bool IsAdmin;
+}
+
+public struct Admin
+{
+    public string Name, Password;
+}
---
+++ Student prerequisite course system/FileOperations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+static public class FileOperations
+{
+    static public class UsersFile
+    {
+        static public void WriteUser(Student s)
+        {
+            throw new NotImplementedException();
+        }
+        static public bool CheckUser(Student s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    static public class AdminFile
+    {
+        static public void WriteAdmin(Admin s)
+        {
+            throw new NotImplementedException();
+        }
+        static public void CheckAdmin(Admin s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    static public class TreeFile
+    {
+        //Delayed till tree is done
+    }
+    static public class Users_SubjectsFile
+    {
+        static public void UpdateUser(Student s, Course c)
+        {
+            throw new NotImplementedException();
+        }
+        static public Course[] GetSubjects(Student s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    static public class Subjects_UsersFile
+    {
+        static public void UpdateSubject(Course c, Student s)
+        {
+            throw new NotImplementedException();
+        }
+        static public Student[] GetStudents(Course c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
