a1fdadd8c0fa45711fb3a9ea58ca06b2ddafd525
naseba <33930286+naseba@users.noreply.github.com>
2017-12-01 01:34:47 -1000

    Update FileOperations.cs

--- Student prerequisite course system/FileOperations.cs
+++ Student prerequisite course system/FileOperations.cs
@@ -7,24 +7,79 @@ static public class FileOperations
     {
         static public void WriteUser(Student s)
         {
-            throw new NotImplementedException();
+            FileStream FW = new FileStream("User.Txt", FileMode.Append);
+            StreamWriter SW = new StreamWriter(FW);
+
+            SW.WriteLine(s.ID + "@" + s.Name + "@" + s.Password + "@" + s.Academic_Year);
+
+            SW.Close();
         }
+        
         static public bool CheckUser(Student s)
         {
-            throw new NotImplementedException();
+            FileStream FR = new FileStream("User.Txt", FileMode.Open);
+            StreamReader SR = new StreamReader(FR);
+            bool found = false;
+
+            while ((SR.Peek() != -1) && (found == false))
+            {
+
+                string Recored = SR.ReadLine(); 
+                string[] field;
+                field = Recored.Split('@');
+                string ID = field[0];
+                string Name = field[1];
+                string password = field[2];
+                string Academic_Year = field[3];
+
+                if ((s.Name.CompareTo(Name) == 0) && (s.Password.CompareTo(password) == 0))
+                    found = true;
+            }
+             SR.Close();
+            if (found == true)
+                return true;
+            else
+                return false;
+           
         }
     }
+    
     static public class AdminFile
     {
         static public void WriteAdmin(Admin s)
         {
-            throw new NotImplementedException();
+             FileStream FW = new FileStream("Admin.Txt", FileMode.Append);
+            StreamWriter SW = new StreamWriter(FW);
+
+            SW.WriteLine(s.Name + "@" + s.Password);
+
+            SW.Close();
         }
         static public void CheckAdmin(Admin s)
         {
-            throw new NotImplementedException();
+            FileStream FR = new FileStream("Admin.Txt", FileMode.Open);
+            StreamReader SR = new StreamReader(FR);
+            bool found = false;
+
+            while ((SR.Peek() != -1) && (found == false))
+            {
+                string Recored = SR.ReadLine(); 
+                string[] field;
+                field = Recored.Split('@');
+                string Name = field[0];
+                string password = field[1];
+
+                if ((s.Name.CompareTo(Name) == 0) && (s.Password.CompareTo(password) == 0))
+                    found = true
+            }
+             SR.Close();
+            if (found == true)
+                return true;
+            else
+                return false;
         }
     }
+    
     static public class CoursesFile
     {
         static public void AddCourse(Course c)
